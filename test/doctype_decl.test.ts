8414b303e005a5c9202e9b4d6e0c6e5492939eb6
FedericoCarboni <fr3ddydev@gmail.com>
2024-04-20 08:52:50 +0000

    test: test doctypedecl thoroughly

--- test/doctype_decl.test.ts
+++ test/doctype_decl.test.ts
@@ -35,12 +35,11 @@ class DoctypeDeclReader implements SaxReader {
   }
 }
 
-function getDoctypeDecl(input: string | string[]) {
+function getDoctypeDecl(...chunks: string[]) {
   const docReader = new DoctypeDeclReader();
   const parser = new SaxParser(docReader);
-  const inputs = typeof input === "string" ? [input] : input;
-  for (const inp of inputs) {
-    parser.write(inp);
+  for (const chunk of chunks) {
+    parser.write(chunk);
   }
   return docReader.doctypeDecl;
 }
@@ -68,5 +67,35 @@ describe("doctypedecl", function() {
         '<!DOCTYPE doctypeName PUBLIC "/public/dtd" "-//DTD/something" [ <![IGNORE[  ]]> ] >',
       ),
     ).to.have.property("name", "doctypeName");
+  });
+  it("wf: doctype with intSubset and ExternalID quoted with apostrophes", function() {
+    expect(
+      getDoctypeDecl(
+        "<!DOCTYPE doctypeName PUBLIC \"/public/dtd\" '-//DTD/something' [ <![IGNORE[  ]]> ] >",
+      ),
+    ).to.have.property("name", "doctypeName");
+  });
+  it("wf: DOCTYPE with an astral character name", function() {
+    expect(getDoctypeDecl("<!DOCTYPE \u{1F000}\u{1F001}\u{1F002}>"))
+      .to.have.property("name", "\u{1F000}\u{1F001}\u{1F002}");
+  });
+  it("wf: DOCTYPE with values split across chunks", function() {
+    expect(
+      getDoctypeDecl(
+        "<!DOCTYPE  ",
+        "doctyp",
+        "eName ",
+        ' PUBLIC "/public',
+        '/dtd" "-//DTD/something" [ <![IGNORE[  ]]> ] >',
+      ),
+    ).to.have.property("name", "doctypeName");
+  });
+  it("not-wf: DOCTYPE with no name", function() {
+    expect(() => getDoctypeDecl("<!DOCTYPE  >"))
+      .to.throw().and.have.property("code", "INVALID_DOCTYPE_DECL");
+  });
+  it("not-wf: Invalid DOCTYPE start", function() {
+    expect(() => getDoctypeDecl("<!DOCTYP doctypName >"))
+      .to.throw().and.have.property("code", "INVALID_CDATA");
   });
 });
