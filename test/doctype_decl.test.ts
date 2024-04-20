84a6edd00d710f0780cc5c1af2f13344a26ed901
FedericoCarboni <fr3ddydev@gmail.com>
2024-04-20 08:54:45 +0000

    test: uniform naming

--- test/doctype_decl.test.ts
+++ test/doctype_decl.test.ts
@@ -61,25 +61,25 @@ describe("doctypedecl", function() {
     ))
       .to.have.property("name", "doctypeName");
   });
-  it("wf: doctype with intSubset and ExternalID", function() {
+  it("wf: doctypedecl with intSubset and ExternalID", function() {
     expect(
       getDoctypeDecl(
         '<!DOCTYPE doctypeName PUBLIC "/public/dtd" "-//DTD/something" [ <![IGNORE[  ]]> ] >',
       ),
     ).to.have.property("name", "doctypeName");
   });
-  it("wf: doctype with intSubset and ExternalID quoted with apostrophes", function() {
+  it("wf: doctypedecl with intSubset and ExternalID quoted with apostrophes", function() {
     expect(
       getDoctypeDecl(
         "<!DOCTYPE doctypeName PUBLIC \"/public/dtd\" '-//DTD/something' [ <![IGNORE[  ]]> ] >",
       ),
     ).to.have.property("name", "doctypeName");
   });
-  it("wf: DOCTYPE with an astral character name", function() {
+  it("wf: doctypedecl with an astral character name", function() {
     expect(getDoctypeDecl("<!DOCTYPE \u{1F000}\u{1F001}\u{1F002}>"))
       .to.have.property("name", "\u{1F000}\u{1F001}\u{1F002}");
   });
-  it("wf: DOCTYPE with values split across chunks", function() {
+  it("wf: doctypedecl with values split across chunks", function() {
     expect(
       getDoctypeDecl(
         "<!DOCTYPE  ",
@@ -90,11 +90,11 @@ describe("doctypedecl", function() {
       ),
     ).to.have.property("name", "doctypeName");
   });
-  it("not-wf: DOCTYPE with no name", function() {
+  it("not-wf: doctypedecl with no name", function() {
     expect(() => getDoctypeDecl("<!DOCTYPE  >"))
       .to.throw().and.have.property("code", "INVALID_DOCTYPE_DECL");
   });
-  it("not-wf: Invalid DOCTYPE start", function() {
+  it("not-wf: doctypedecl with invalid start", function() {
     expect(() => getDoctypeDecl("<!DOCTYP doctypName >"))
       .to.throw().and.have.property("code", "INVALID_CDATA");
   });
