bfe6a48bc06ad0e317d3f1d767d67c884e7d6348
federicocarboni <48333714+federicocarboni@users.noreply.github.com>
2025-10-06 12:34:07 +0200

    feat: namespaces

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ import {
   isNameStartChar,
   isWhiteSpace,
 } from "./chars.ts";
-import {createSaxError, SaxErrorCode} from "./error.ts";
-import {parseXmlDecl} from "./xml_decl.ts";
+import { createSaxError, SaxErrorCode } from "./error.ts";
+import { parseXmlDecl } from "./xml_decl.ts";
 
-export {SaxDecoder} from "./encoding.ts";
-export {isSaxError, type SaxError, type SaxErrorCode} from "./error.ts";
+export { SaxDecoder } from "./encoding.ts";
+export { isSaxError, type SaxError, type SaxErrorCode } from "./error.ts";
 
 /**
  * XML Declaration (XMLDecl).
@@ -958,7 +958,7 @@ export class SaxParser {
     ) {
       return undefined;
     }
-    return {name: this.element_, publicId, systemId};
+    return { name: this.element_, publicId, systemId };
   }
 
   // @internal
@@ -1881,7 +1881,7 @@ export class SaxParser {
     if (attlist === undefined) {
       return;
     }
-    for (const [attribute, {default_}] of attlist) {
+    for (const [attribute, { default_ }] of attlist) {
       if (default_ !== undefined && !this.attributes_.has(attribute)) {
         if (this.attributes_.size >= this.maxAttributes_) {
           throw this.createSaxError_("LIMIT_EXCEEDED");
@@ -2209,7 +2209,7 @@ export class SaxParser {
       // Unparsed entities cannot be referenced anywhere.
       // WFC: Parsed Entity
       if (entityValue === EntityDecl.UNPARSED) {
-        throw this.createSaxError_("UNPARSED_ENTITY", {entity: this.entity_});
+        throw this.createSaxError_("UNPARSED_ENTITY", { entity: this.entity_ });
       }
       // Attribute values
       // WFC: No External Entity References
@@ -2217,7 +2217,7 @@ export class SaxParser {
         this.otherState_ === State.START_TAG_ATTR_VALUE_QUOTED &&
         entityValue === EntityDecl.EXTERNAL
       ) {
-        throw this.createSaxError_("EXTERNAL_ENTITY", {entity: this.entity_});
+        throw this.createSaxError_("EXTERNAL_ENTITY", { entity: this.entity_ });
       }
       // Allow the application to set a default value for an entity not
       // declared in internal markup declarations.
@@ -2561,5 +2561,190 @@ export class SaxParser {
       ++this.index_;
     }
     return this.index_ !== this.chunk_.length;
+  }
+}
+
+export interface QName {
+  name: string;
+  localName: string;
+  prefix?: string | undefined;
+  uri?: string | undefined;
+}
+
+export interface AttributeNs extends QName {
+  value: string;
+}
+
+interface AttributesNs {
+  /**
+   * Returns the attribute by name and namespace URI if necessary.
+   * @param name
+   * @param uri
+   */
+  get(name: string, uri?: string | undefined): AttributeNs | undefined;
+  values(): IterableIterator<AttributeNs>;
+  [Symbol.iterator](): IterableIterator<AttributeNs>;
+}
+
+// @internal
+class AttributesNs implements AttributesNs {
+  // @internal
+  private map_ = new Map<string, AttributeNs>();
+  // @internal
+  clear_() {
+    this.map_.clear();
+  }
+  // @internal
+  add_(attribute: AttributeNs) {
+    this.map_.set(attribute.uri != null ? `${attribute.uri}:${attribute.localName}` : attribute.localName, attribute);
+  }
+  get(name: string, uri?: string | undefined): AttributeNs | undefined {
+    // Name must be NCName
+    if (name.indexOf(':') !== -1)
+      return undefined;
+    return this.map_.get(uri != null ? `${uri}:${name}` : name);
+  }
+  values(): IterableIterator<AttributeNs> {
+    return this.map_.values();
+  }
+  [Symbol.iterator]() {
+    return this.map_.values();
+  }
+}
+
+// Export AttributesNs only as a type. The class is an implementation detail.
+export { type AttributesNs };
+
+export interface SaxReaderNs extends Omit<SaxReader, "start" | "empty" | "end"> {
+  start(name: QName, attributes: AttributesNs): void;
+  empty(name: QName, attributes: AttributesNs): void;
+  end(name: QName): void;
+}
+
+export class SaxParserNs implements SaxReader {
+  // prefix name -> URI (the last is the most recent)
+  // @internal
+  private namespaces_ = new Map<string, string[]>([
+    ["xml", ["http://www.w3.org/XML/1998/namespace"]],
+    ["xmlns", ["http://www.w3.org/2000/xmlns/"]]
+  ]);
+  // Depth number -> prefix names
+  // @internal
+  private nsPrefixes_ = new Map<number, string[]>();
+  // @internal
+  private depth_ = 0;
+  // Reused across calls
+  // @internal
+  private attributes_ = new AttributesNs();
+  // @internal
+  private qName_: QName = {
+    name: "",
+    prefix: undefined,
+    localName: "",
+    uri: undefined,
+  };
+  // @internal
+  private reader_: SaxReaderNs;
+  constructor(reader: SaxReaderNs) {
+    this.reader_ = reader;
+    this.xml = this.reader_.xml != null ? this.xml : undefined;
+    this.doctype = this.reader_.doctype != null ? this.doctype : undefined;
+    this.processingInstruction = this.reader_.processingInstruction != null ? this.processingInstruction : undefined;
+    this.comment = this.reader_.comment != null ? this.comment : undefined;
+    this.getGeneralEntity = this.reader_.getGeneralEntity != null ? this.getGeneralEntity : undefined;
+    this.entityRef = this.reader_.entityRef != null ? this.entityRef : undefined;
+  }
+  xml?(declaration: XmlDeclaration) {
+    return this.reader_.xml!(declaration);
+  }
+  doctype?(doctype: Doctype) {
+    return this.reader_.doctype!(doctype);
+  }
+  processingInstruction?(target: string, content: string) {
+    return this.reader_.processingInstruction!(target, content);
+  }
+  comment?(text: string) {
+    return this.reader_.comment!(text);
+  }
+  getGeneralEntity?(entityName: string) {
+    return this.reader_.getGeneralEntity!(entityName);
+  }
+  entityRef?(entityName: string) {
+    return this.reader_.entityRef!(entityName);
+  }
+  // @internal
+  private parseQName_(name: string, isAttribute: boolean): QName {
+    const prefixLen = name.indexOf(":");
+    if (prefixLen === 0 || name.indexOf(":", prefixLen + 1) !== -1)
+      throw new Error("empty NCName not allowed");
+    const prefix = prefixLen === -1 ? undefined : name.slice(0, prefixLen);
+    const localName = name.slice(prefixLen + 1);
+    const uris = prefix === undefined && isAttribute ? undefined : this.namespaces_.get(prefix ?? "");
+    if (prefix !== undefined && uris === undefined)
+      throw new Error("namespace not declared");
+    const uri = uris !== undefined ? uris[uris.length - 1] : undefined;
+    this.qName_.name = name;
+    this.qName_.localName = localName;
+    this.qName_.prefix = prefix;
+    this.qName_.uri = uri;
+    return this.qName_;
+  }
+  // @internal
+  private handleAttributes_(attributes: ReadonlyMap<string, string>): AttributesNs {
+    this.attributes_.clear_()
+    const prefixes = [];
+    for (const [name, value] of attributes.entries()) {
+      // Collect namespaces first
+      if (name !== "xmlns" && name.slice(0, 6) !== "xmlns:")
+        continue;
+      if (name.indexOf(":", 6) !== -1)
+        throw new Error("invalid prefix");
+      const prefix = name.slice(6);
+      if (prefix === "xmlns" || prefix === "xml")
+        throw new Error("namespace not valid");
+      let ns = this.namespaces_.get(prefix);
+      if (ns === undefined) {
+        ns = [value];
+        this.namespaces_.set(prefix, ns);
+      }
+      prefixes.push(prefix);
+    }
+    if (prefixes.length !== 0)
+      this.nsPrefixes_.set(this.depth_, prefixes);
+    for (const [name, value] of attributes.entries()) {
+      const attribute = Object.assign({ value }, this.parseQName_(name, true));
+      this.attributes_.add_(attribute);
+    }
+    return this.attributes_;
+  }
+  // @internal
+  private popPrefixes_() {
+    const prefixes = this.nsPrefixes_.get(this.depth_);
+    this.nsPrefixes_.delete(this.depth_);
+    this.depth_--;
+    const length = prefixes?.length ?? 0;
+    for (let i = 0; i < length; i++) {
+      this.namespaces_.get(prefixes![i]!)!.pop();
+    }
+  }
+  start(name: string, attributes: ReadonlyMap<string, string>) {
+    this.depth_ += 1;
+    const attributesNs = this.handleAttributes_(attributes);
+    const qName = this.parseQName_(name, false);
+    this.reader_.start(qName, attributesNs);
+  }
+  empty(name: string, attributes: ReadonlyMap<string, string>) {
+    this.depth_ += 1;
+    const attributesNs = this.handleAttributes_(attributes);
+    const qName = this.parseQName_(name, false);
+    this.reader_.empty(qName, attributesNs);
+    this.popPrefixes_();
+  }
+  end(name: string) {
+    this.reader_.end(this.parseQName_(name, false));
+    this.popPrefixes_();
+  }
+  text(text: string) {
+    return this.reader_.text(text);
   }
 }
