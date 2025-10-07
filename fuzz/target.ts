06335c3ce980850eb433f6b619a079e9b3e5311e
federicocarboni <>
2025-10-07 17:13:14 +0000

    feat: update fuzz harness to verify char by char input works correctly

--- fuzz/target.ts
+++ fuzz/target.ts
@@ -1,23 +1,37 @@
-import {isSaxError, SaxParser} from "../src/index.ts";
+import { SaxError, SaxParser } from "../src/index.ts";
+import { CanonicalXmlWriter } from "../test/canonical_xml.ts";
 
-// TODO: add correctness test through Canonical XML
 export function fuzz(data: Buffer) {
   const str = data.toString("utf-8");
-  const saxParser = new SaxParser({
-    processingInstruction() {},
-    comment() {},
-    entityRef() {},
-    start() {},
-    empty() {},
-    end() {},
-    text() {},
-  });
+  const writer1 = new CanonicalXmlWriter();
+  const writer2 = new CanonicalXmlWriter();
+  const parser1 = new SaxParser(writer1);
+  const parser2 = new SaxParser(writer2);
+  let error1: SaxError | undefined;
+  let error2: SaxError | undefined;
   try {
-    saxParser.write(str);
-    saxParser.end();
+    parser1.write(str);
+    parser1.end();
   } catch (error) {
-    if (!isSaxError(error)) {
+    if (!(error instanceof SaxError)) {
       throw error;
     }
+    error1 = error;
+  }
+  try {
+    for (const c of str) {
+      parser2.write(c);
+    }
+    parser2.end();
+  } catch (error) {
+    if (!(error instanceof SaxError)) {
+      throw error;
+    }
+    error2 = error;
+  }
+  if (writer1.output !== writer2.output || error1?.code !== error2?.code) {
+    throw new Error(
+      `--- FULL: ---\n${writer1.output}\n${error1}\n\n--- CHAR BY CHAR: ---\n${writer2.output}\n${error2}`
+    );
   }
 }
