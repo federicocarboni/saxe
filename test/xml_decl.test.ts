20c7e6d0e47051e12bb63165795833f3fee72e7c
federicocarboni <48333714+federicocarboni@users.noreply.github.com>
2025-10-13 10:39:49 +0200

    refactor: InvalidXMLDecl to InvalidXmlDecl

--- test/xml_decl.test.ts
+++ test/xml_decl.test.ts
@@ -32,55 +32,55 @@ describe("XML Declaration", function() {
   it("not-wf: XMLDecl requires VersionInfo before EncodingDecl", function() {
     expect(() => getXmlDecl('<?xml encoding="UTF-8" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl requires VersionInfo before EncodingDecl or SDDecl", function() {
     expect(() => getXmlDecl('<?xml standalone="yes" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl requires EncodingDecl to be absent or precede SDDecl", function() {
     expect(() =>
       getXmlDecl('<?xml version="1.0" standalone="yes" encoding="UTF-8" ?>')
     )
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl requires space before EncodingDecl", function() {
     expect(() => getXmlDecl('<?xml version="1.0"encoding="UTF-8" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl requires space before SDDecl", function() {
     expect(() =>
       getXmlDecl('<?xml version="1.0" encoding="UTF-8"standalone="yes" ?>')
     )
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl VersionInfo may appear only once", function() {
     expect(() => getXmlDecl('<?xml version="1.0" version="1.0" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl EncodingDecl may appear only once", function() {
     expect(() =>
       getXmlDecl('<?xml version="1.0" encoding="UTF-8" encoding="UTF-8" ?>')
     )
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl SDDecl may appear only once", function() {
     expect(() =>
       getXmlDecl('<?xml version="1.0" standalone="yes" standalone="yes" ?>')
     )
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl VersionInfo must be quoted", function() {
     expect(() => getXmlDecl("<?xml version=1.0 ?>"))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("wf: XMLDecl VersionInfo may be quoted with apostrophe", function() {
     expect(getXmlDecl("<?xml version='1.0' ?><root/>")).deep.equals({
@@ -101,12 +101,12 @@ describe("XML Declaration", function() {
   it("not-wf: XMLDecl VersionNum must match production", function() {
     expect(() => getXmlDecl('<?xml version=" 1.0" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl EncodingDecl must be quoted", function() {
     expect(() => getXmlDecl('<?xml version="1.0" encoding=UTF-8 ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("wf: XMLDecl EncodingDecl may have space before and after equals sign", function() {
     expect(
@@ -129,12 +129,12 @@ describe("XML Declaration", function() {
   it("not-wf: XMLDecl EncName must match production", function() {
     expect(() => getXmlDecl('<?xml version="1.0" encoding=" UTF-8" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl SDDecl must be quoted", function() {
     expect(() => getXmlDecl('<?xml version="1.0" standalone=yes ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("wf: XMLDecl SDDecl may be quoted with apostrophe", function() {
     expect(
@@ -166,22 +166,22 @@ describe("XML Declaration", function() {
       getXmlDecl("<?xml version=\"1.0\" standalone='value' ?><root/>")
     )
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl rejects unknown long pseudo-attributes", function() {
     expect(() => getXmlDecl('<?xml version="1.0" some-other-name="value" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl rejects unknown pseudo-attributes", function() {
     expect(() => getXmlDecl('<?xml version="1.0" other-name="value" ?>'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl ends at '?>' not '?'", function() {
     expect(() => getXmlDecl('<?xml version="1.0" ? >'))
       .to.throw()
-      .and.have.property("name", "InvalidXMLDecl");
+      .and.have.property("name", "InvalidXmlDecl");
   });
   it("not-wf: XMLDecl ends at '?>' not '>'", function() {
     expect(() => getXmlDecl('<?xml version="1.0" >'))
