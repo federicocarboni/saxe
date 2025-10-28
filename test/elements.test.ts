1f98828a1b71015b670a123fc4e43cde501d3e60
federicocarboni <>
2025-10-28 17:49:43 +0000

    test: add tests for tags and attributes

--- test/elements.test.ts
+++ test/elements.test.ts
@@ -1,19 +1,71 @@
 import {expect} from "chai";
+import {SaxError} from "../src/error.ts";
 import {toCanonical} from "./template.ts";
 
+describe("tags", function() {
+  it("wf: regular start tag end tag", function() {
+    expect(toCanonical("<name></name>")).equals("<name></name>");
+  });
+  it("wf: empty tag", function() {
+    expect(toCanonical("<name />")).equals("<name></name>");
+    expect(toCanonical("<name/>")).equals("<name></name>");
+  });
+  it("not-wf: space before name", function() {
+    expect(() => toCanonical("< name></name>")).throws().instanceOf(SaxError)
+      .includes({name: "InvalidStartTag"});
+  });
+});
+
+describe("attributes", function() {
+  it("wf: regular attributes", function() {
+    expect(toCanonical('<name attr="value"></name>')).equals(
+      '<name attr="value"></name>',
+    );
+  });
+  it("wf: attributes in empty tag", function() {
+    expect(toCanonical('<name attr="value"/>')).equals(
+      '<name attr="value"></name>',
+    );
+    expect(toCanonical('<name attr="value" />')).equals(
+      '<name attr="value"></name>',
+    );
+  });
+  it("wf: space before =", function() {
+    expect(toCanonical("<name attr ", " ", '="value"/>')).equals(
+      '<name attr="value"></name>',
+    );
+  });
+  it("wf: space after =", function() {
+    expect(toCanonical('<name attr= "value"/>')).equals(
+      '<name attr="value"></name>',
+    );
+  });
+  it("wf: space before and after =", function() {
+    expect(toCanonical('<name attr = "value"/>')).equals(
+      '<name attr="value"></name>',
+    );
+  });
+  it("not-wf: no =", function() {
+    expect(() => toCanonical('<name attr "value"/>'))
+      .throws()
+      .instanceOf(SaxError)
+      .includes({name: "InvalidStartTag"});
+  });
+});
+
 describe("text content", function() {
-  it("allows '>'", function() {
+  it("wf: allows '>'", function() {
     expect(toCanonical("<name>></name>")).equals("<name>&gt;</name>");
   });
-  it("refuses ']]>'", function() {
+  it("not-wf: refuses ']]>'", function() {
     expect(() => toCanonical("<name>]]></name>")).throws();
     expect(() => toCanonical("<name>]]]]]]></name>")).throws();
   });
-  it("refuses ']]>' with multiple precedin brackets", function() {
+  it("not-wf: refuses ']]>' with multiple precedin brackets", function() {
     expect(() => toCanonical("<name>]]></name>")).throws();
     expect(() => toCanonical("<name>]]]]]]></name>")).throws();
   });
-  it("allows ']'", function() {
+  it("wf: allows ']'", function() {
     expect(toCanonical("<root>]]]]<name>></name></root>")).equals(
       "<root>]]]]<name>&gt;</name></root>",
     );
