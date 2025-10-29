cd452ff179e852868702613574048248e61894f0
federicocarboni <>
2025-10-29 18:12:05 +0000

    chore: formatting

--- test/w3c_test_suite.test.ts
+++ test/w3c_test_suite.test.ts
@@ -1,7 +1,12 @@
 // XML Conformance Test Suite harness
 // https://www.w3.org/XML/Test/
 
-import {nsTestCases, runNsTest, runTest, testCases} from "./w3c_test_suite/harness.ts";
+import {
+  nsTestCases,
+  runNsTest,
+  runTest,
+  testCases,
+} from "./w3c_test_suite/harness.ts";
 
 describe("XML W3C Conformance Test Suite", function() {
   for (const [type, testCaseList] of testCases) {
