8e708b967cdc96845d5dbf53856223f1be399cdd
federicocarboni <>
2025-10-30 11:10:01 +0000

    chore: ignore fuzz target

--- eslint.config.js
+++ eslint.config.js
@@ -14,58 +14,67 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-  extends: compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-  ),
+export default defineConfig([
+  {
+    extends: compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:import/recommended",
+    ),
 
-  languageOptions: {
-    parser: tsParser,
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      "import": _import,
+      jsdoc,
+    },
+
+    rules: {
+      "no-prototype-builtins": "off",
+      "no-proto": "error",
+
+      "no-constant-condition": ["error", {
+        checkLoops: false,
+      }],
+
+      "eqeqeq": ["error", "always", {
+        null: "ignore",
+      }],
+
+      "curly": ["error", "multi-line"],
+      "no-throw-literal": "error",
+      "no-with": "error",
+      "import/no-cycle": "error",
+      "import/no-self-import": "error",
+      "import/no-default-export": "error",
+
+      "import/extensions": ["error", {
+        ts: "always",
+      }],
+
+      "@typescript-eslint/no-empty-object-type": ["error", {
+        allowInterfaces: "with-single-extends",
+      }],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": ["error", {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      }],
+    },
   },
-
-  plugins: {
-    "@typescript-eslint": typescriptEslint,
-    "import": _import,
-    jsdoc,
-  },
-
-  rules: {
-    "no-prototype-builtins": "off",
-    "no-proto": "error",
-
-    "no-constant-condition": ["error", {
-      checkLoops: false,
-    }],
-
-    "eqeqeq": ["error", "always", {
-      null: "ignore",
-    }],
-
-    "curly": ["error", "multi-line"],
-    "no-throw-literal": "error",
-    "no-with": "error",
-    "import/no-cycle": "error",
-    "import/no-self-import": "error",
-    "import/no-default-export": "error",
-
-    "import/extensions": ["error", {
-      ts: "always",
-    }],
-
-    "@typescript-eslint/no-empty-object-type": ["error", {
-      allowInterfaces: "with-single-extends",
-    }],
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/no-unused-vars": ["error", {
-      args: "all",
-      argsIgnorePattern: "^_",
-      caughtErrors: "all",
-      caughtErrorsIgnorePattern: "^_",
-      destructuredArrayIgnorePattern: "^_",
-      varsIgnorePattern: "^_",
-      ignoreRestSiblings: true,
-    }],
-  },
-}, globalIgnores(["eslint.config.js", "lib", "coverage", "docs"])]);
+  globalIgnores([
+    "eslint.config.js",
+    "lib",
+    "coverage",
+    "docs",
+    "fuzz/target.js",
+  ]),
+]);
