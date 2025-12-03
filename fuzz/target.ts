0703bd33f38b1c036a0fd9fa42911b54aeb7ab55
federicocarboni <>
2025-12-03 21:07:29 +0000

    chore: format

--- fuzz/target.ts
+++ fuzz/target.ts
@@ -17,8 +17,14 @@ export function fuzz(data: Buffer) {
   }
   const writer1 = new CanonicalXmlWriter();
   const writer2 = new CanonicalXmlWriter();
-  const parser1 = new SaxParser(writer1, {incrementalText: true, dtd: "process"});
-  const parser2 = new SaxParser(writer2, {incrementalText: true, dtd: "process"});
+  const parser1 = new SaxParser(writer1, {
+    incrementalText: true,
+    dtd: "process",
+  });
+  const parser2 = new SaxParser(writer2, {
+    incrementalText: true,
+    dtd: "process",
+  });
   let error1: SaxError | undefined;
   let error2: SaxError | undefined;
   try {
